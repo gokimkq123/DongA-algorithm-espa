4d592cf89b39419a8573caa5720b47004ef627c0
kim geon han <gokimkq111@naver.com>
2022-10-06 00:22:04 +0900

    문제 9

---
+++ coin.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define all(v) v.begin(), v.end()
+#define INF 2e9
+using namespace std;
+
+typedef long long ll;
+typedef pair<int,int> pa;
+typedef tuple<int,int,int,int> tu;
+typedef vector<int> vc;
+
+int main() {
+    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+
+}
